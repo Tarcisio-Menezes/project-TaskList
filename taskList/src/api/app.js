714b53c816e93ecafaae0c8472a72221fd2a95a9
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-01 15:12:12 -0300

    adiciona rota em app

--- taskList/src/api/app.js
+++ taskList/src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const usersRoute = require('../routes/usersRoutes');
 
 const app = express();
 app.use(express.json());
+
+app.use(usersRoute);
 
 module.exports = app;
