b12684bd6fac6de90012ac05d79389cf01ecd92b
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-01 16:19:54 -0300

    adiciona middleware de erro em app

--- taskList/src/api/app.js
+++ taskList/src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const usersRoute = require('../routes/usersRoutes');
 const loginRoute = require('../routes/loginRoutes');
 const middlewareUsersError = require('../middlewares/usersError');
+const middlewareLoginError = require('../middlewares/loginError');
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,6 @@ app.use(usersRoute);
 app.use(middlewareUsersError);
 
 app.use(loginRoute);
+app.use(middlewareLoginError);
 
 module.exports = app;
