49df7d9476709e673db284f64cc6d1a7619c4714
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-01 15:01:38 -0300

    controllers: adiciona joi

---
+++ taskList/src/controllers/usersController.js
@@ -0,0 +1,21 @@
+const joi = require('joi');
+const service = require('../services/usersService');
+
+const registerUser = async (req, res, next) => {
+  const { error } = joi.object({
+    name: joi.string().required(),
+    password: joi.string().required(),
+  }).validate(req.body);
+
+  if (error) return next(error);
+
+  const { name, password } = req.body;
+
+  const result = await service.registerUser({ name, password });
+  if (result.error) return next(result.error);
+  return res.status(201).json(result);
+};
+
+module.exports = {
+  registerUser,
+};
