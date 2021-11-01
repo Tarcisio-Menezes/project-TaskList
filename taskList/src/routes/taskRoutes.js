1ef85464a3070736d569e5dbe2c91f9b0cf2af6b
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-01 17:55:34 -0300

    adiciona middleware jwt na rota de tasks

--- taskList/src/routes/taskRoutes.js
+++ taskList/src/routes/taskRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('../controllers/taskController');
+const middlewareAuth = require('../middlewares/auth');
 
-router.post('/tasks', controller.taskRegister);
+router.post('/tasks', middlewareAuth, controller.taskRegister);
 
 module.exports = router;
