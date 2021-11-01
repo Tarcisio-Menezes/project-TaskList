506f43a0797358a114c1bed9501170a830c37227
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-01 16:11:22 -0300

    adiciona controller de login

---
+++ taskList/src/controllers/loginController.js
@@ -0,0 +1,21 @@
+const joi = require('joi');
+const service = require('../services/loginService');
+
+const userLogin = async (req, res, next) => {
+  const { error } = joi.object({
+    name: joi.string().required(),
+    password: joi.string().required(),
+  }).validate(req.body);
+
+  if (error) return next(error);
+
+  const { name, password } = req.body;
+
+  const token = await service.userLogin(name, password);
+  if (token.error) return next(token.error);
+  return res.status(200).json(token);
+};
+
+module.exports = {
+  userLogin,
+};
