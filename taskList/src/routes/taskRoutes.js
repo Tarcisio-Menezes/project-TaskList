38f8ed56b646b39c65ed555f850cf933b0a07d4f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-02 14:45:49 -0300

    adiciona rota para edição

--- taskList/src/routes/taskRoutes.js
+++ taskList/src/routes/taskRoutes.js
@@ -5,5 +5,6 @@ const middlewareAuth = require('../middlewares/auth');
 router.post('/tasks', middlewareAuth, controller.taskRegister);
 router.get('/tasks', middlewareAuth, controller.getAllTasksUser);
 router.get('/tasks/order', middlewareAuth, controller.getAllAlphOrder);
+router.put('/tasl/:id', middlewareAuth, controller.taskEdit);
 
 module.exports = router;
