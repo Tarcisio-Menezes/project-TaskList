e1856ab6f1bbd313ad38b815e0711b6ca90de50f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-02 14:46:56 -0300

    corrige rota para edição

--- taskList/src/routes/taskRoutes.js
+++ taskList/src/routes/taskRoutes.js
@@ -5,6 +5,6 @@ const middlewareAuth = require('../middlewares/auth');
 router.post('/tasks', middlewareAuth, controller.taskRegister);
 router.get('/tasks', middlewareAuth, controller.getAllTasksUser);
 router.get('/tasks/order', middlewareAuth, controller.getAllAlphOrder);
-router.put('/tasl/:id', middlewareAuth, controller.taskEdit);
+router.put('/tasks/:id', middlewareAuth, controller.taskEdit);
 
 module.exports = router;
