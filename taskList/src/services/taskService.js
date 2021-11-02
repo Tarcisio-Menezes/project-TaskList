9e230b0680e139b3945b815d73d223f2855774e2
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-02 13:50:52 -0300

    taskService: buscar por tarefas do usuário

--- taskList/src/services/taskService.js
+++ taskList/src/services/taskService.js
@@ -2,6 +2,9 @@ const taskModel = require('../models/taskModel');
 
 const registerTask = async (task, userName) => taskModel.taskRegister(task, userName);
 
+const getAllTask = async (userName) => taskModel.getAll(userName);
+
 module.exports = {
   registerTask,
+  getAllTask,
 };
