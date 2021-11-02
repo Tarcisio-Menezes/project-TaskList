704f0d87980c66be3414ea061f77e7b87314c1ef
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-02 14:41:57 -0300

    taskService: adiciona regra de negócio para edição de tarefas

--- taskList/src/services/taskService.js
+++ taskList/src/services/taskService.js
@@ -6,7 +6,15 @@ const getAllTask = async (userName) => taskModel.getAll(userName);
 
 const getAllAlphOrder = async (userName) => taskModel.getAllAlphabeticalOrder(userName);
 
-const taskEdit = async (task, userName) => taskModel.taskEdit(task, userName);
+const taskEdit = async (task, userName) => {
+  const { id } = task;
+  const taskById = await taskModel.searchTaskById(id);
+  if (taskById.userName.equals(userName)) {
+    return taskModel.taskEdit(task, userName);
+  } return ({
+    error: { code: 'invalid' },
+  });  
+};
 
 module.exports = {
   registerTask,
