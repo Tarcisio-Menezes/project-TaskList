eeda55b8a515ee544df445cd0b8775546d3f384f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-02 14:23:54 -0300

    taskService: adiciona regra de negócio para edição de task

--- taskList/src/controllers/taskController.js
+++ taskList/src/controllers/taskController.js
@@ -1,4 +1,5 @@
 const joi = require('joi').extend(require('@joi/date'));
+const { serialize } = require('bson');
 const service = require('../services/taskService');
 
 const taskRegister = async (req, res, next) => {
@@ -36,8 +37,30 @@ const getAllAlphOrder = async (req, res, next) => {
   return res.status(200).json(getAll);
 };
 
+const taskEdit = async (req, res, next) => {
+  const { name } = req.user;
+  const { id } = req.params;
+
+  const { error } = joi.object({
+    title: joi.string().required(),
+    description: joi.string().required(),
+    date: joi.date().format('DD/MM/YY').utc().required(),
+    status: joi.string().required(),
+  }).validate(req.body);
+
+  if (error) return next(error);
+
+  const { title, description, date, status } = req.body;
+
+  const task = { id, title, description, date, status };
+  const edit = await service.taskEdit(task, name);
+  if (edit.error) return next(edit.error);
+  return res.status(200).json(edit);
+};
+
 module.exports = {
   taskRegister,
   getAllTasksUser,
   getAllAlphOrder,
+  taskEdit,
 };
--- taskList/src/services/taskService.js
+++ taskList/src/services/taskService.js
@@ -6,8 +6,11 @@ const getAllTask = async (userName) => taskModel.getAll(userName);
 
 const getAllAlphOrder = async (userName) => taskModel.getAllAlphabeticalOrder(userName);
 
+const taskEdit = async (task, userName) => taskModel.taskEdit(task, userName);
+
 module.exports = {
   registerTask,
   getAllTask,
   getAllAlphOrder,
+  taskEdit,
 };
