b9c7e912d3761d16fad49beba9e541f33b1be2f5
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-03 18:45:25 -0300

    muda login para createUser - adiciona nova página

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -1,7 +1,8 @@
 import { Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
 import Home from './pages/Home';
-import Login from './pages/Login'
+import Login from './pages/AddUser'
+import AddUser from './pages/AddUser';
 import ErrorPage from './pages/ErrorPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
     <Provider>
       <Switch>
         <Route path="/" component={ Login } />
+        <Route exact path="/create-user" component={ AddUser } />
         <Route exact path="/home" component={ Home } />
         <Route path="/*" component={ ErrorPage } />
       </Switch>
