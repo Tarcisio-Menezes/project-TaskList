10a407a655c1d86f66fbb3da15da10aff2316f1d
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-03 18:55:31 -0300

    adiciona service para fazer login

--- task-list/src/services/APIConnection.js
+++ task-list/src/services/APIConnection.js
@@ -22,3 +22,26 @@ export const userRegister = (userName, pass) => {
       } return console.error(errorOrResponse);
     });
 }
+
+export const userLogin = (userName, pass) => {
+  const body = {
+    name: userName,
+    password: pass,
+  };
+
+  const headers = {
+    'Content-Type': 'application/json',
+    "Access-Control-Allow-Origin": "*"
+  };
+
+  axios.post('http://localhost:3000/login', body, { headers })
+    .then((response) => {
+      return response.data;
+    })
+    .then((data) => data)
+    .catch((errorOrResponse) => {
+      if (errorOrResponse.status) {
+        return console.error(`Request failed with status ${errorOrResponse.status}`);
+      } return console.error(errorOrResponse);
+    });
+}
