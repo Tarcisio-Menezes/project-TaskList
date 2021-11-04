0add0d01303bdbc49fa615e1ac63e92c855330bd
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-04 11:49:31 -0300

    lista tarefas do usuário

--- task-list/src/pages/Home.js
+++ task-list/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
 import axios from 'axios';
 import MainContext from '../context/MainContext';
 
@@ -34,20 +35,37 @@ function Home () {
 
   console.log(tasks);
 
-  const userIsValid = () => {
+  const getValidUserTask = () => {
     if (!token) {
       return <h2>Oops, seu token é inválido ou expirou, por favor faça login novamente.</h2>
     } return (
       <section>
         <h2>Olá {userName}, espero que esteja bem!</h2>
         <h4>Suas tarefas são:</h4>
+        { tasks.map((task) => {
+          return (
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>{ task.title }</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Status: { task.status }</Card.Subtitle>
+              <Card.Text>
+                { task.description }
+              </Card.Text>
+              <button href="#">Editar</button>
+              <button href="#">Deletar</button>
+            </Card.Body>
+          </Card>
+          );
+        })
+      };
+      <br/>
       </section>
     );
   };
 
   return (
     <section>
-      { userIsValid() }
+      { getValidUserTask() }
     </section>
   );
 };
