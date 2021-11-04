3c86aac0626a0cfe054b56c1392d296c6549ba8e
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-04 13:44:20 -0300

    adiciona funcionalidade de add tarefas

--- task-list/src/pages/Home.js
+++ task-list/src/pages/Home.js
@@ -1,11 +1,16 @@
-import React, { useContext, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Card, Form } from 'react-bootstrap';
 import axios from 'axios';
 import MainContext from '../context/MainContext';
 
 function Home () {
 
   const { userName, token, setTasks, tasks } = useContext(MainContext);
+  const [taskTitle, setTaskTitle] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+  const [taskStatus, setTaskStatus] = useState('');
+  const [taskDate, setTaskDate] = useState('');
+  const [respost, setRespost] = useState('');
 
   const getAllTasks = async (token) => {
     const headers = {
@@ -26,14 +31,37 @@ function Home () {
       });
   }
 
+  const addTasks = async (token) => {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'authorization':token,
+    };
+
+    const body = {
+      title: taskTitle,
+      description: taskDescription,
+      date: taskDate,
+      status: taskStatus,
+    };
+  
+    axios.post('http://localhost:3000/tasks', body, { headers })
+      .then((response) => {
+        if (response.data) {
+          return setRespost(response.data);
+        } 
+      })
+      .catch((errorOrResponse) => {
+         return errorOrResponse;
+      });
+  }
+
   useEffect(() => {
     async function getTasks() {
       await getAllTasks(token);
     }
     getTasks();
-  }, [token, setTasks]);
-
-  console.log(tasks);
+  }, [token, respost]);
 
   const getValidUserTask = () => {
     if (!token) {
@@ -42,12 +70,21 @@ function Home () {
       <section>
         <h2>Olá {userName}, espero que esteja bem!</h2>
         <h4>Suas tarefas são:</h4>
-        { tasks.map((task) => {
+        { tasks && tasks.map((task, index) => {
           return (
-          <Card style={{ width: '18rem' }}>
+          <Card style={{ width: '18rem' }} key={ index }>
             <Card.Body>
-              <Card.Title>{ task.title }</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Status: { task.status }</Card.Subtitle>
+              <Card.Title>
+                { task.title }
+              </Card.Title>
+              <Card.Subtitle 
+                className="mb-2 text-muted">
+                    Prazo: { task.date }
+              </Card.Subtitle>
+              <Card.Subtitle 
+                className="mb-2 text-muted">
+                  Status: { task.status }
+              </Card.Subtitle>
               <Card.Text>
                 { task.description }
               </Card.Text>
@@ -57,15 +94,57 @@ function Home () {
           </Card>
           );
         })
-      };
-      <br/>
+      }
       </section>
     );
   };
 
+  const addNewTask = () => {
+    if (!token) {
+      return <h2> Nenhuma tarefa disponível.</h2>
+    } return (
+        <section>
+          <h4>Adicionar nova tarefa</h4>
+          <Form.Control
+            type="text"
+            placeholder="Título da nova tarefa"
+            onChange={ ({ target }) => setTaskTitle(target.value) }
+            required
+          />
+          <Form.Control
+            type="text"
+            placeholder="Descrição"
+            onChange={ ({ target }) => setTaskDescription(target.value) }
+            required
+          />
+          <Form.Control
+            type="text"
+            placeholder="Andamento da tarefa"
+            onChange={ ({ target }) => setTaskStatus(target.value) }
+            required
+          />
+          <Form.Control
+            type="text"
+            placeholder="Prazo DD/MM/AA"
+            onChange={ ({ target }) => setTaskDate(target.value) }
+            required
+          />
+          <button
+            onClick={ () => addTasks(token) }
+            disabled={ !taskDate || !taskDescription || !taskTitle || !taskStatus }
+          >
+            Enviar
+          </button>
+        </section>
+    );
+  };
+
+
+
   return (
     <section>
       { getValidUserTask() }
+      { addNewTask() }
     </section>
   );
 };
