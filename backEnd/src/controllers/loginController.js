89ce30fd2f3873fdfb30ea8737f33d79ee8b7a38
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-06 14:07:07 -0300

    adiciona rescue em loginController

--- backEnd/src/controllers/loginController.js
+++ backEnd/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const joi = require('joi');
+const rescue = require('express-rescue');
 const service = require('../services/loginService');
 
-const userLogin = async (req, res, next) => {
+const userLogin =  rescue(async (req, res, next) => {
   const { error } = joi.object({
     name: joi.string().required(),
     password: joi.string().required(),
@@ -14,7 +15,7 @@ const userLogin = async (req, res, next) => {
   const token = await service.userLogin(name, password);
   if (token.error) return next(token.error);
   return res.status(200).json(token);
-};
+});
 
 module.exports = {
   userLogin,
